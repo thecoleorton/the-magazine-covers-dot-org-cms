4ef739ce15442ed7ad00d42de1ebb962ba7e69ff
Cole Orton <co7791@att.com>
2013-11-11 16:02:19 -0600

    refactor /collections to /api

--- express.js
+++ express.js
@@ -12,37 +12,36 @@ app.param('collectionName', function(req, res, next, collectionName){
 })
 
 app.get('/', function(req, res, next) {
-  res.send('please select a collection, e.g., /collections/messages')
+  res.send('please provide a valid api path, e.g., /api/some_valid_api')
 })
 
-app.get('/collections/:collectionName', function(req, res, next) {
+app.get('/api/:collectionName', function(req, res, next) {
   req.collection.find({},{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
     if (e) return next(e)
     res.send(results)
   })
 })
 
-app.post('/collections/:collectionName', function(req, res, next) {
+app.post('/api/:collectionName', function(req, res, next) {
   req.collection.insert(req.body, {}, function(e, results){
     if (e) return next(e)
     res.send(results)
   })
 })
 
-
-app.get('/collections/:collectionName/:id', function(req, res, next) {
+app.get('/api/:collectionName/:id', function(req, res, next) {
   req.collection.findOne({_id: req.collection.id(req.params.id)}, function(e, result){
     if (e) return next(e)
     res.send(result)
   })
 })
-app.put('/collections/:collectionName/:id', function(req, res, next) {
+app.put('/api/:collectionName/:id', function(req, res, next) {
   req.collection.update({_id: req.collection.id(req.params.id)}, {$set:req.body}, {safe:true, multi:false}, function(e, result){
     if (e) return next(e)
     res.send((result===1)?{msg:'success'}:{msg:'error'})
   })
 })
-app.del('/collections/:collectionName/:id', function(req, res, next) {
+app.del('/api/:collectionName/:id', function(req, res, next) {
   req.collection.remove({_id: req.collection.id(req.params.id)}, function(e, result){
     if (e) return next(e)
     res.send((result===1)?{msg:'success'}:{msg:'error'})
